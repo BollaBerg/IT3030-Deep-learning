ffe44256d78962903f2c274439cc63a86b870a13
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-03-01 19:41:30 +0100

    Add main-file, with required code for deliveries
    Currently trains Autoencoder, which runs, but due to being on my
    laptop I haven't ran more than 2 epochs. The loss DOES go down, so
    that's at least a good thing. Will hopefully run on my home PC later
    tonight, and we'll see if it works :)

---
+++ Project 2 - Probabilistic AI/main.py
@@ -0,0 +1,61 @@
+from src.autoencoder import AutoEncoder, device
+from supplied_files.stacked_mnist import DataMode, StackedMNISTData
+from supplied_files.verification_net import VerificationNet
+
+import torch
+from torch.nn import BCEWithLogitsLoss
+from torch.optim import Adam
+
+
+def train_autoencoder():
+    LEARNING_RATE = 1e-3
+    DATAMODE = DataMode.MONO_BINARY_COMPLETE
+    EPOCHS = 500
+
+    model = AutoEncoder().to(device)
+    loss_fn = BCEWithLogitsLoss()
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+    data_generator = StackedMNISTData(DATAMODE)
+    verification_net = VerificationNet()
+
+    for epoch in range(EPOCHS):
+        print(f"########## EPOCH {epoch + 1} ##########", end="\n\n")
+
+        ### TRAINING ###
+        model.train()
+        X, y = data_generator.get_full_data_set()
+        X = X.to(device)
+        
+        X_hat = model(X)
+        loss = loss_fn(X_hat, X)
+
+        # Backpropagation - this time implemented by Pytorch (i.e. it works)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+        ### TESTING ###
+        model.eval()
+        X_test, y_test = data_generator.get_full_data_set(training=False)
+        X_hat_test = model(X_test)
+        
+        test_loss = loss_fn(X_hat_test, X_test)
+
+        predictability_data = torch.permute(
+            X_hat_test, (0, 2, 3, 1)
+        ).detach().numpy()
+
+        predictability, accuracy = verification_net.check_predictability(
+            predictability_data, y_test, tolerance=0.8
+        )
+        print(f"Test loss:      {test_loss:.5f}")
+        print(f"Predictability: {100*predictability:.2f}%")
+        print(f"Accuracy:       {100*accuracy:.2f}%")
+
+
+
+
+
+if __name__ == "__main__":
+    train_autoencoder()
