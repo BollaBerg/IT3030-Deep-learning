2cfc521bab7c6c825d69e6a37e9ddd2284675edb
Andreas Berg <berg.andreasb@gmail.com>
2022-03-02 14:04:43 +0100

    Batch color data in main to not overrun CPU memory
    Note that I don't really know how much faster this is than non-batch CPU
    training, as I haven't checked. Seems a little faster, but not by much,
    sadly

--- Project 2 - Probabilistic AI/main.py
+++ Project 2 - Probabilistic AI/main.py
@@ -64,7 +64,7 @@ def train_autoencoder_color():
     model = AutoEncoder(channels=3).to(device)
     loss_fn = BCEWithLogitsLoss()
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
-    data_generator = StackedMNISTData(DATAMODE)
+    data_generator = StackedMNISTData(DATAMODE, default_batch_size=10000)
     verification_net = VerificationNet()
 
     for epoch in range(EPOCHS):
@@ -72,31 +72,32 @@ def train_autoencoder_color():
 
         ### TRAINING ###
         model.train()
-        X, y = data_generator.get_full_data_set()
-        X = X.to(device)
-        
-        X_hat = model(X)
-        loss = loss_fn(X_hat, X)
+        for X, y in data_generator.batch_generator():
+            X = X.to(device)
+            
+            X_hat = model(X)
+            loss = loss_fn(X_hat, X)
 
-        # Backpropagation - this time implemented by Pytorch (i.e. it works)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Backpropagation - this time implemented by Pytorch (i.e. it works)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
 
         ### TESTING ###
         model.eval()
         X_test, y_test = data_generator.get_full_data_set(training=False)
+        X_test = X_test.to(device)
         X_hat_test = model(X_test)
         
         test_loss = loss_fn(X_hat_test, X_test)
 
         predictability_data = torch.permute(
             X_hat_test, (0, 2, 3, 1)
-        ).detach().numpy()
+        ).cpu().detach().numpy()
 
         predictability, accuracy = verification_net.check_predictability(
-            predictability_data, y_test, tolerance=0.8
+            predictability_data, y_test, tolerance=0.5
         )
         print(f"Test loss:      {test_loss:.5f}")
         print(f"Predictability: {100*predictability:.2f}%")
