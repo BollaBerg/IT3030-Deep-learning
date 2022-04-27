c8ca21b9fa381aeb1ef835d9fcaf7df3a9c49088
Andreas Berg <berg.andreasb@gmail.com>
2022-04-27 11:43:51 +0200

    Add docstrings to Preprocesser. Minor changes

--- Project 3 - Imbalance forecasting/src/preprocessing/preprocesser.py
+++ Project 3 - Imbalance forecasting/src/preprocessing/preprocesser.py
@@ -1,13 +1,29 @@
 import pandas as pd
 
-from src.helpers.core import list_without_element
 from src.helpers.path import DATA_PATH
 from src.preprocessing.functions import clamp_column
 from src.preprocessing.standardizer import Standardizer
 
+
+class NotFittedError(Exception):
+    pass
+
+
 class Preprocesser:
+    """
+    Organizer of all required preprocessing for the given datasets and tasks.
+
+    Typical usage:
+        >>> training_data = pd.DataFrame(...)
+        >>> validation_data = pd.DataFrame(...)
+        >>> preprocesser = Preprocesser()
+        >>> training_data = preprocesser.fit_transform(training_data)
+        >>> validation_data = preprocesser.transform(validation_data)
+    
+    """
     keep_columns = ["start_time"]
     transform_columns = ["hydro", "micro", "thermal", "wind", "total", "sys_reg", "flow"]
+    if_fitted = False
 
     def __init__(self,
                  min_y_value: float = -1000,
@@ -19,28 +35,59 @@ class Preprocesser:
     
 
     def fit(self, data: pd.DataFrame):
+        """Fit the Preprocesser to a DataFrame.
+
+        Args:
+            data (pd.DataFrame): Data which the Preprocesser should fit to.
+        """
         for column in self.transform_columns:
             self.transformers[column] = Standardizer()
             self.transformers[column].fit(data[column])
+        
+        self.is_fitted = True
     
 
     def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        """Transform a DataFrame using the fitted Preprocesser.
+
+        Note: Requires that the Preprocesser has previously been fitted.
+
+        Args:
+            data (pd.DataFrame): Data to transform
+
+        Raises:
+            NotFittedError: Raised if Preprocesser has not been fitted
+
+        Returns:
+            pd.DataFrame: Transformed data
+        """
+        if not self.is_fitted:
+            raise NotFittedError("Preprocesser has not been fitted yet.")
+
         output = data[self.keep_columns].copy()
 
         for column in self.transform_columns:
             output[column] = self.transformers[column].transform(data[column])
         
-        try:
-            output["y"] = clamp_column(
-                data, column="y", lower=self.min_y_value, upper=self.max_y_value
-            )
-        except KeyError:
-            pass
+        output["y"] = clamp_column(
+            data, column="y", lower=self.min_y_value, upper=self.max_y_value
+        )
+
+        # TODO: Create new features
         
         return output
     
 
     def fit_transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        """Fit the Preprocesser to a DataFrame, then transform the DataFrame
+
+        Args:
+            data (pd.DataFrame): Data the Preprocesser should fit to, then
+                transform
+
+        Returns:
+            pd.DataFrame: Transformed DataFrame
+        """
         self.fit(data)
         return self.transform(data)
 
@@ -54,7 +101,3 @@ if __name__ == "__main__":
     processed = preprocesser.fit_transform(data)
     print("FIT_TRANSFORM")
     print(processed.describe())
-
-    processed_test = preprocesser.transform(data.drop(columns=["y"]))
-    print("TRANSFORM")
-    print(processed_test.describe())
