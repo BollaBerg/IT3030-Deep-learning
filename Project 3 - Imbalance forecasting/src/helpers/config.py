1e76dd79a80da356d099d6ee7bf9a0c1d10f18f1
Andreas Berg <berg.andreasb@gmail.com>
2022-05-01 20:29:24 +0200

    Enable more preprocessing steps in config

--- Project 3 - Imbalance forecasting/src/helpers/config.py
+++ Project 3 - Imbalance forecasting/src/helpers/config.py
@@ -12,6 +12,12 @@ class TrainingConfig:
 @dataclass
 class DataConfig:
     sequence_length: int
+    time_of_day: bool
+    time_of_week: bool
+    time_of_year: bool
+    last_day_y: bool
+    two_last_day_y: bool
+
 
 @dataclass
 class ModelConfig:
@@ -40,6 +46,11 @@ def read_config(path: str) -> tuple[TrainingConfig, DataConfig, ModelConfig]:
     )
     data_config = DataConfig(
         sequence_length = data.get("data").get("sequence_length"),
+        time_of_day = data.get("data").get("time_of_day", False),
+        time_of_week = data.get("data").get("time_of_week", False),
+        time_of_year = data.get("data").get("time_of_year", False),
+        last_day_y = data.get("data").get("last_day_y", False),
+        two_last_day_y = data.get("data").get("two_last_day_y", False),
     )
     model_config = ModelConfig(
         lstm_depth = data.get("model").get("lstm_depth"),
