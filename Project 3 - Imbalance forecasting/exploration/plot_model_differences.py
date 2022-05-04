b4de73190fc30a36cbfab17e9f9adbeefd857999
Andreas Berg <berg.andreasb@gmail.com>
2022-05-04 22:08:02 +0200

    Prepare for comparing model differences

---
+++ Project 3 - Imbalance forecasting/exploration/plot_model_differences.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+from src.helpers.path import ROOT_PATH
+
+def plot_losses(loss_list: list):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))
+    ax.set_title("Hyperparameter differences")
+    ax.set_yscale("log")
+    ax.set_ylabel("log(loss), MSE")
+    ax.set_xlabel("Epoch")
+
+    colormap = plt.get_cmap("Paired").colors
+
+    for i, (model_name, loss_location) in enumerate(loss_list):
+        with open(loss_location, "r") as file:
+            losses = file.readlines()
+            losses_float = [float(loss) for loss in losses]
+
+            ax.plot(losses_float, label=model_name, color=colormap[i])
+    
+    ax.legend()
+    fig.savefig(ROOT_PATH / "plots/model_differences.png")
+
+
+if __name__ == "__main__":
+    loss_list = [
+        ("All options off", "models/losses/1_all_off.losses")
+    ]
+    plot_losses(loss_list)
