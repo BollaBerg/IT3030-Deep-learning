1481ce1227f7de5a9d647eb23e0ebfa05b879e9f
Andreas Berg <berg.andreasb@gmail.com>
2022-05-05 00:17:50 +0200

    Minor changes to model plotting

--- Project 3 - Imbalance forecasting/exploration/plot_model_differences.py
+++ Project 3 - Imbalance forecasting/exploration/plot_model_differences.py
@@ -4,7 +4,7 @@ from src.helpers.path import ROOT_PATH
 
 def plot_losses(loss_list: list):
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))
-    ax.set_title("Hyperparameter differences")
+    ax.set_title("Hyperparameter differences on validation loss")
     ax.set_yscale("log")
     ax.set_ylabel("log(loss), MSE")
     ax.set_xlabel("Epoch")
@@ -24,6 +24,7 @@ def plot_losses(loss_list: list):
 
 if __name__ == "__main__":
     loss_list = [
-        ("All options off", "models/losses/1_all_off.losses")
+        ("All options off", "models/losses/1_all_off.losses"),
+        ("Time columns on", "models/losses/2_time_on.losses"),
     ]
     plot_losses(loss_list)
