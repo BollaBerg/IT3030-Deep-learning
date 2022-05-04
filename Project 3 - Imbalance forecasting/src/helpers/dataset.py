2ba8a5a78fa4b56c6d8256f13b27f1b06e99ca86
Andreas Berg <berg.andreasb@gmail.com>
2022-05-04 17:13:01 +0200

    Fix error in __getitem__ re: target. Add FutureDataset

--- Project 3 - Imbalance forecasting/src/helpers/dataset.py
+++ Project 3 - Imbalance forecasting/src/helpers/dataset.py
@@ -17,8 +17,32 @@ class TimeSeriesDataset(Dataset):
     
     def __getitem__(self, index: int):
         inputs = self.inputs[index: index + self.window]
-        target = self.targets[index + self.window]
+        target = self.targets[index + self.window - 1]
         return inputs, target
     
     def __len__(self):
         return len(self.inputs) - self.window
+
+class FutureDataset(Dataset):
+    """
+    Dataset class, adapted for time series data with future prediction
+
+    Returns a window of data, with width = window + future length
+
+    """
+    def __init__(self, inputs: Tensor, targets: Tensor, past_window: int, future_window: int):
+        self.inputs = inputs
+        self.targets = targets
+        self.past_window = past_window
+        self.future_window = future_window
+    
+    def __getitem__(self, index: int):
+        inputs = self.inputs[index: index + self.past_window + self.future_window]
+        targets = self.targets[
+            index + self.past_window - 1:
+            index + self.past_window + self.future_window - 1
+        ]
+        return inputs, targets
+    
+    def __len__(self):
+        return len(self.inputs) - self.past_window - self.future_window
