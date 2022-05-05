62585f84541b696233c42d2786e18e81aa8dcc40
Andreas Berg <berg.andreasb@gmail.com>
2022-05-05 15:55:08 +0200

    Move future prediction from paths to own method

--- Project 3 - Imbalance forecasting/src/demo.py
+++ Project 3 - Imbalance forecasting/src/demo.py
@@ -1,16 +1,13 @@
 from pathlib import Path
 
-import pandas as pd
 from torch.nn import MSELoss
-from torch.utils.data import DataLoader
 
-from src.helpers.config import read_config
-from src.helpers.dataset import FutureDataset
-from src.helpers.path import DATA_PATH, ROOT_PATH
-from src.helpers.plotting import plot_future_predictions, plot_zoomed_future_predictions
-from src.future_prediction import predict_into_future
-from src.lstm import LSTM
-from src.preprocessing import Preprocesser, split_data, pd_to_tensor
+from src.helpers.path import ROOT_PATH
+from src.helpers.plotting import (
+    plot_future_predictions, plot_zoomed_future_predictions,
+    plot_predictions_from_single_starts
+)
+from src.future_prediction import predict_future_from_paths
 
 
 def _format_steps(step: int) -> str:
@@ -28,52 +25,10 @@ def _format_steps(step: int) -> str:
 
 def demo(config_path: Path, model_path: Path, data_path: Path):
     future_steps = int(120 / 5)      # 2 hours = 120 min / 5 min timesteps
-    config = read_config(config_path)
-    input_size = (
-        8   # Standard, from the actual input data
-        + (2 if config.data.time_of_day else 0)    # time_of_day has two cols
-        + (2 if config.data.time_of_week else 0)   # time_of_week has two cols
-        + (2 if config.data.time_of_year else 0)   # time_of_year has two cols
-        + (1 if config.data.last_day_y else 0)     # last_day_y has one col
-        + (1 if config.data.two_last_day_y else 0) # two_last_day_y has one col
-    )
-    model = LSTM(
-        input_size = input_size,
-        lstm_depth=config.model.lstm_depth,
-        hidden_layers=config.model.hidden_layers
-    )
-    model.load_model(model_path)
-
-    data = pd.read_csv(data_path, parse_dates=["start_time"])
-
-    # Read training data only to fit preprocesser
-    _training_data = pd.read_csv(DATA_PATH / "train.csv", parse_dates=["start_time"])
-    preprocesser = Preprocesser(
-        time_of_day=config.data.time_of_day,
-        time_of_week=config.data.time_of_week,
-        time_of_year=config.data.time_of_year,
-        last_day_y=config.data.last_day_y,
-        two_last_day_y=config.data.two_last_day_y
-    )
-    preprocesser.fit(_training_data)
-
-    processed_data = preprocesser.transform(data)
-    inputs_df, target_df = split_data(processed_data)
-    inputs = pd_to_tensor(inputs_df)
-    targets = pd_to_tensor(target_df)
-
-    dataset = FutureDataset(inputs, targets,
-        past_window=config.data.sequence_length,
-        future_window=future_steps
-    )
-    dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=False)
-
     loss_fn = MSELoss(reduction="mean")
 
-    predictions = predict_into_future(
-        model, dataloader,
-        timesteps_into_future=future_steps,
-        reverse_target=preprocesser.reverse_y
+    predictions = predict_future_from_paths(
+        config_path, model_path, data_path, future_steps, loss_fn
     )
 
     for i, future_step in enumerate(predictions):
@@ -85,8 +40,15 @@ def demo(config_path: Path, model_path: Path, data_path: Path):
             loss_str=f"Loss: {loss}"
         )
     
+    # Plot a zoomed window of 2-hour predictions
     plot_zoomed_future_predictions(
         predictions[-1][0], predictions[-1][1],
         title="Zoomed predictions, 2 hours into the future",
         savepath=ROOT_PATH / "plots/LSTM_zoomed.png"
+    )
+
+    # Plot four plots of the previous target, then predictions over two hours
+    plot_predictions_from_single_starts(
+        predictions,
+        savepath=ROOT_PATH / "plots/LSTM_over_time.png"
     )
--- Project 3 - Imbalance forecasting/src/future_prediction.py
+++ Project 3 - Imbalance forecasting/src/future_prediction.py
@@ -1,8 +1,15 @@
+from pathlib import Path
+
+import pandas as pd
 import torch
+from torch.nn import MSELoss
 from torch.utils.data import DataLoader
 
+from src.helpers.config import read_config
 from src.helpers.dataset import FutureDataset
+from src.helpers.path import DATA_PATH
 from src.lstm import LSTM
+from src.preprocessing import Preprocesser, split_data, pd_to_tensor
 
 def predict_into_future(model: LSTM,
                         dataloader: DataLoader,
@@ -67,6 +74,61 @@ def predict_into_future(model: LSTM,
     
     return output
 
+
+def predict_future_from_paths(
+        config_path: Path,
+        model_path: Path,
+        data_path: Path,
+        future_steps: int = int(120 / 5),
+        loss_fn = MSELoss(reduction="mean"),
+    ):
+    config = read_config(config_path)
+    input_size = (
+        8   # Standard, from the actual input data
+        + (2 if config.data.time_of_day else 0)    # time_of_day has two cols
+        + (2 if config.data.time_of_week else 0)   # time_of_week has two cols
+        + (2 if config.data.time_of_year else 0)   # time_of_year has two cols
+        + (1 if config.data.last_day_y else 0)     # last_day_y has one col
+        + (1 if config.data.two_last_day_y else 0) # two_last_day_y has one col
+    )
+    model = LSTM(
+        input_size = input_size,
+        lstm_depth=config.model.lstm_depth,
+        hidden_layers=config.model.hidden_layers
+    )
+    model.load_model(model_path)
+
+    data = pd.read_csv(data_path, parse_dates=["start_time"])
+
+    # Read training data only to fit preprocesser
+    _training_data = pd.read_csv(DATA_PATH / "train.csv", parse_dates=["start_time"])
+    preprocesser = Preprocesser(
+        time_of_day=config.data.time_of_day,
+        time_of_week=config.data.time_of_week,
+        time_of_year=config.data.time_of_year,
+        last_day_y=config.data.last_day_y,
+        two_last_day_y=config.data.two_last_day_y
+    )
+    preprocesser.fit(_training_data)
+
+    processed_data = preprocesser.transform(data)
+    inputs_df, target_df = split_data(processed_data)
+    inputs = pd_to_tensor(inputs_df)
+    targets = pd_to_tensor(target_df)
+
+    dataset = FutureDataset(inputs, targets,
+        past_window=config.data.sequence_length,
+        future_window=future_steps
+    )
+    dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=False)
+
+    loss_fn = MSELoss(reduction="mean")
+
+    predictions = predict_into_future(
+        model, dataloader,
+        timesteps_into_future=future_steps,
+        reverse_target=preprocesser.reverse_y
+    )
 
 
 if __name__ == "__main__":
