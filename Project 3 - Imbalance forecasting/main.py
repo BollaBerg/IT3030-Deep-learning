110038d98cd0883feb1a9025f469ff8fc669dfb8
Andreas Berg <berg.andreasb@gmail.com>
2022-05-06 02:02:50 +0200

    Change defaults to best long-term models

--- Project 3 - Imbalance forecasting/main.py
+++ Project 3 - Imbalance forecasting/main.py
@@ -64,12 +64,12 @@ if __name__ == "__main__":
         if len(sys.argv) >= 4:
             config_path = pathlib.Path(sys.argv[3])
         else:
-            config_path = ROOT_PATH / "models/configs/7_day_year_targets_more_memory.yml"
+            config_path = ROOT_PATH / "models/configs/11_6_with_smaller_depth.yml"
 
         if len(sys.argv) >= 5:
             model_path = pathlib.Path(sys.argv[4])
         else:
-            model_path = ROOT_PATH / "models/7_day_year_targets_more_memory.pt"
+            model_path = ROOT_PATH / "models/11_6_with_smaller_depth.pt"
         
         demo(config_path, model_path, data_path)
 
@@ -87,7 +87,7 @@ if __name__ == "__main__":
         if len(sys.argv) >= 5:
             model_path = pathlib.Path(sys.argv[4])
         else:
-            model_path = ROOT_PATH / "models/9_task_2_from_6.pt"
+            model_path = ROOT_PATH / "models/9_task_2_from_6_epoch_21.pt"
         
         demo(config_path, model_path, data_path, task_2=True)
 
