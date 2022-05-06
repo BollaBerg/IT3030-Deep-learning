3c4377a08234aadab49967fef7cbc54f210c35c5
Andreas Berg <berg.andreasb@gmail.com>
2022-05-06 02:02:13 +0200

    Plot model differences for all models

--- Project 3 - Imbalance forecasting/exploration/plot_model_differences.py
+++ Project 3 - Imbalance forecasting/exploration/plot_model_differences.py
@@ -1,15 +1,17 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 
 from src.helpers.path import ROOT_PATH
 
-def plot_losses(loss_list: list):
+def plot_losses(loss_list: list, savepath: Path):
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))
     ax.set_title("Hyperparameter differences on validation loss")
     ax.set_yscale("log")
     ax.set_ylabel("log(loss), MSE")
     ax.set_xlabel("Epoch")
 
-    colormap = plt.get_cmap("Set1").colors
+    colormap = plt.get_cmap("Set1").colors + plt.get_cmap("Accent").colors
 
     for i, (model_name, loss_location) in enumerate(loss_list):
         with open(loss_location, "r") as file:
@@ -19,11 +21,11 @@ def plot_losses(loss_list: list):
             ax.plot(losses_float, label=model_name, color=colormap[i])
     
     ax.legend()
-    fig.savefig(ROOT_PATH / "plots/model_differences.png")
+    fig.savefig(savepath)
 
 
 if __name__ == "__main__":
-    loss_list = [
+    task_1_loss_list = [
         ("All options off", "models/losses/1_all_off.losses"),
         ("Time columns on", "models/losses/2_time_on.losses"),
         ("Past targets on", "models/losses/3_hanging_y.losses"),
@@ -31,6 +33,16 @@ if __name__ == "__main__":
         ("Time of day and week", "models/losses/5_day_week.losses"),
         ("Time of day, year + past targets", "models/losses/6_day_year_hanging.losses"),
         ("Time of day, year + past targets (30 steps)", "models/losses/7_day_year_targets_more_memory.losses"),
-        ("Randomness in last_y", "models/losses/8_randomness_last_y.losses")
+        ("Randomness in last_y", "models/losses/8_randomness_last_y.losses"),
+        ("Time of day, year + past targets (dropout)", "models/losses/10_6_with_dropout.losses"),
+        ("Time of day, year + past targets (only 1 LSTMcell)", "models/losses/11_6_with_smaller_depth.losses"),
+        ("Time of day, year + past targets (only 1 LSTMcell + dropout)", "models/losses/12_6_with_smaller_depth_with_dropout.losses"),
     ]
-    plot_losses(loss_list)
+    plot_losses(task_1_loss_list, ROOT_PATH / "plots/model_differences_task_1.png")
+
+    task_2_loss_list = [
+        ("All options off", "models/losses/13_task_2_all_off.losses"),
+        ("Time of day, year + past targets", "models/losses/9_task_2_from_6.losses"),
+    ]
+    plot_losses(task_2_loss_list, ROOT_PATH / "plots/model_differences_task_2.png")
+
--- Project 3 - Imbalance forecasting/exploration/plot_model_prediction_differences.py
+++ Project 3 - Imbalance forecasting/exploration/plot_model_prediction_differences.py
@@ -9,10 +9,11 @@ from src.helpers.path import DATA_PATH, ROOT_PATH
 
 def plot_prediction_losses(
             model_list: list,
+            savepath: str,
         ):
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
     loss_fn = MSELoss(reduction="mean")
-    cmap = plt.get_cmap("Set1").colors
+    cmap = plt.get_cmap("Set1").colors + plt.get_cmap("Accent").colors
 
     for i, model in enumerate(tqdm(model_list, unit="model")):
         name, config_path, model_path = model
@@ -34,14 +35,13 @@ def plot_prediction_losses(
     ax.set_yscale("log")
     ax.legend()
 
-    savepath = ROOT_PATH / "plots/model_future_differences.png"
     fig.savefig(savepath)
     print(f"Model prediction differences saved to {savepath}")
 
 
 
 if __name__ == "__main__":
-    paths = [
+    task_1_list = [
         ("All options off", "models/configs/1_all_off.yml", "models/1_all_off.pt"),
         ("Time columns on", "models/configs/2_time_on.yml", "models/2_time_on.pt"),
         ("Past targets on", "models/configs/3_hanging_y.yml", "models/3_hanging_y.pt"),
@@ -50,5 +50,19 @@ if __name__ == "__main__":
         ("Time of day, year + past targets", "models/configs/6_day_year_hanging.yml", "models/6_day_year_hanging.pt"),
         ("Time of day, year + past targets (30 steps)", "models/configs/7_day_year_targets_more_memory.yml", "models/7_day_year_targets_more_memory.pt"),
         ("Randomness in last_y", "models/configs/8_randomness_last_y.yml", "models/8_randomness_last_y.pt"),
+        ("Time of day, year + past targets (30 steps)", "models/configs/10_6_with_dropout.yml", "models/10_6_with_dropout.pt"),
+        ("Time of day, year + past targets (only 1 LSTMcell)", "models/configs/11_6_with_smaller_depth.yml", "models/11_6_with_smaller_depth.pt"),
+        (
+            "Time of day, year + past targets (only 1 LSTMcell + dropout)",
+            "models/configs/12_6_with_smaller_depth_with_dropout.yml",
+            "models/12_6_with_smaller_depth_with_dropout.pt"
+        ),
     ]
-    plot_prediction_losses(paths)
+    plot_prediction_losses(task_1_list, ROOT_PATH / "plots/model_future_differences_task_1.png")
+    
+    task_2_list = [
+        ("All options off", "models/configs/13_task_2_all_off.yml", "models/13_task_2_all_off.pt"),
+        ("Time of day, year + past targets (epoch 13)", "models/configs/9_task_2_from_6.yml", "models/9_task_2_from_6.pt"),
+        ("Time of day, year + past targets (epoch 21)", "models/configs/9_task_2_from_6.yml", "models/9_task_2_from_6_epoch_21.pt"),
+    ]
+    plot_prediction_losses(task_2_list, ROOT_PATH / "plots/model_future_differences_task_2.png")
